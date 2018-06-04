76f4e71cf57831b02eec107affd2171a9ffc3a20
Dmitry Zharinov <zharinovdm@yandex.ru>
2018-06-04 22:23:40 +0300

    Add templates

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,18 @@ from app import app
 @app.route('/index')
 
 def index():
-	user = {'username': 'Dmitry'}
-	return render_template('index.html', title='Home', user = user)
+    user = {'username': 'Dmitry'}
+    posts = [
+        {
+            'author': {'username': 'm1rko'},
+            'body': 'Как безопасно программировать в bash'
+        },
+        {
+            'author': {'username': 'mkulesh'},
+            'body': 'Отладочная плата STM32F4 в форм-факторе Raspberry Pi'
+        }, 
+        {
+            'author': {'username': 'mi5ha6in'},
+            'body': 'CSRF-уязвимости все еще актуальны'
+        }]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
