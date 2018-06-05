e5cfc204cde3624c7ffd3bbc3f3a6dd1b61ed8ad
Dmitriy Zharinov <zharinovdm@yandex.ru>
2018-06-05 15:29:54 +0300

    update routes

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Dmitry'}
-    posts = [
-        {
-            'author': {'username': 'm1rko'},
-            'body': 'Как безопасно программировать в bash'
-        },
-        {
-            'author': {'username': 'mkulesh'},
-            'body': 'Отладочная плата STM32F4 в форм-факторе Raspberry Pi'
-        }, 
-        {
-            'author': {'username': 'mi5ha6in'},
-            'body': 'CSRF-уязвимости все еще актуальны'
-        }]
-    return render_template('index.html', title = 'Home', user = user, posts = posts)
+    posts = [{'author': {'username': 'csrfb'},
+             'body': 'CSRF-уязвимости все еще актуальны'},
+             {'author': {'username': 'ghub'},
+             'body': 'GitHub теперь официально принадлежит Microsoft'},
+             {'author': {'username': 'GlobalSign_admin'},
+             'body': 'АНБ предложило стандарт шифрования для устройств Интернета вещей, но ISO его отвергло'},
+             {'author': {'username': 'SMDP'},
+             'body': 'Стрелочные LED-часы для обучения пайке SMD компонентов'},
+             {'author': {'username': 'trienergy'},
+             'body': 'Токамак JET начинает новую дейтерий-тритиевую кампанию'}]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Вход', form=form)
+    if form.validate_on_submit():
+        flash('Пользователь {} успешно авторизован с опцией "запомнить меня" {}'.format(
+                form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
 
+    return render_template('login.html', title='Вход', form=form)
