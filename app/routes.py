5d55f0cab079b69f66e01aedcc349aa8035cad16
EC2 Default User <ec2-user@ip-172-31-38-161.ec2.internal>
2019-08-14 14:03:18 +0000

    создание функционала логина

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,40 @@
 from app import flask_app
-from flask import render_template, redirect, flash, url_for
+from flask import request, render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 @flask_app.route('/')
 @flask_app.route('/index')
+@login_required
 def index():
-    title = 'Welcome'
-    user = {'username': 'Dmitry'}
+    title = 'Home Page'
     posts = [{
-        'author': user,
+        'author': current_user,
         'body': "Lorem ipsum"}]
 
-    return render_template('index.html', user = user, title = title, posts = posts)
+    return render_template('index.html', title = title, posts = posts)
 
 
 @flask_app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.rememberMe.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first() #получить объект пользователя из базы
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@flask_app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
